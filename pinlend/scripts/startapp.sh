e26a847224f5f90e5c71120fee508f7aead5a146
Nguyen Thong <thong@adaptivemedia.com.au>
2018-06-26 15:37:01 +1000

    Build scripts

---
+++ pinlend/scripts/startapp.sh
@@ -0,0 +1,7 @@
+!/bin/bash
+# cd /home/ec2-user/herobeservice
+# source /home/ec2-user/.nvm/nvm.sh
+# node -e "console.log('Running Node.js ' + process.version)"
+# cd /home/ec2-user/herobeservice
+# npm install
+# npm start
